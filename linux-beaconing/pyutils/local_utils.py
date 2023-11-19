d704c80dccc07e4ab0b9fe014c50bd38213474ef
cac2107 <cac2106@rit.edu>
2023-11-18 20:11:57 -0500

    bugfixin

--- linux-beaconing/pyutils/local_utils.py
+++ linux-beaconing/pyutils/local_utils.py
@@ -114,6 +114,7 @@ def get_all_services_cmd(message):
         final = ""
         for service in services:
             service = service.encode('utf-8').decode('utf-8')
+            print(service)
             service_status = get_service_status(service)
             # Assuming get_service_status returns a string, adjust the encoding if needed
             service_status = service_status.encode("utf-8").decode("utf-8")
